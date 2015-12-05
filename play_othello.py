fb2ed0e35ce41ce2a0d2e19b9355e75f5e0847b1
Darshan Parajuli <dparajul@uci.edu>
2015-12-04 19:14:10 -0800

    update

--- play_othello.py
+++ play_othello.py
@@ -2,7 +2,7 @@ import gui
 
 
 def main() -> None:
-    setup_window = gui.SetupWindow(False)
+    setup_window = gui.SetupWindow(False) # False: start as the mainwindow in the beginning
     setup_window.start()
 
 if __name__ == '__main__':
